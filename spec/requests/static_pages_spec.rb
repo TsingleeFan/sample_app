f077b652aa81e9e3c1023cb4274a460df863f66d
tsinglee <qingli1992@163.com>
2014-12-08 13:01:46 +0800

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,15 +4,20 @@ describe "Static Pages" do
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   
   describe "Home page" do
+ 
     it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Sample App')
     end
-    
-    it "should have the title 'Home'" do
+   
+    it "should have the base title" do
       visit '/static_pages/home'
-      page.should have_selector('title',
-                        :text => "#{base_title} | Home")
+      page.should have_selector('title',:text => "#{base_title}")
+    end
+   
+    it "should not have a custom page title" do
+        visit '/static_pages/home'
+        page.should_not have_selector('title', :text => '| Home')
     end
   end
 
@@ -23,8 +28,7 @@ describe "Static Pages" do
     end
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('title',
-                        :text => "{base_title} | Help")
+      page.should have_selector('title',:text => "#{base_title} | Help")
     end
   end
   
@@ -35,8 +39,7 @@ describe "Static Pages" do
       end
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector('title',
-                        :text => "{base_title} | About Us")
+      page.should have_selector('title',:text => "#{base_title} | About Us")
     end
   end
     
@@ -47,8 +50,7 @@ describe "Static Pages" do
       end
     it "should have the title 'Contact Us'" do
       visit '/static_pages/contact'
-      page.should have_selector('title',
-                        :text => "{base_title} | Contact Us")
+      page.should have_selector('title',:text => "#{base_title} | Contact Us")
     end
   end
   
